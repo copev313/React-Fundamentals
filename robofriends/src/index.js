c06221a606e67912b9ffdd413fbdb32d99172a5d
copev313 <copev313@gmail.com>
2022-02-13 15:24:08 -0500

    added tachyons + Card component

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+import 'tachyons';
+import { robots } from './robots';
+
+import Card from './Card.js';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    { robots.map(robot => 
+        (<Card  key={robot.id}
+                id={robot.id}
+                name={robot.name}
+                email={robot.email}  />)
+      )
+    }
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ robofriends/src/robots.js
@@ -0,0 +1,35 @@
+
+export const robots= [
+
+    {
+        id: 1,
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@email.com"
+    },
+    {
+        id: 2,
+        name: "Ervin Howell",
+        username: "Antonette",
+        email: "Shanna@emaim.com"
+    },
+    {
+        id: 3,
+        name: "Clementine Bauch",
+        username: "Samantha",
+        email: "Nah@email.com"
+    },
+    {
+        id: 4,
+        name: "Patricia Lebsack",
+        username: "Karianne",
+        email: "patpat@email.com"
+    },
+    {
+        id: 5,
+        name: "Chelsey Dietrich",
+        username: "Kamren",
+        email: "kamy_kam@email.com"
+    },
+
+]
